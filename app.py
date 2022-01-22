b0413845dc78945a5865128b55104076ff543707
Andry D. Diaz Leon - Musala <andry.diaz@musala.com>
2022-01-22 14:46:34 +0200

    Fixing bugs

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask_blog_app import create_app
 
-APP = create_app()
+app = create_app()
 
 if __name__ == '__main__':
     # APP.run(host='0.0.0.0', port=5000, debug=True)
-    APP.run(debug=False)
+    app.run(debug=False)
